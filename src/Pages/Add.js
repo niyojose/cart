1c3c949115eb5646e6058a1976172ef0c19981ec
jojo <90638640+niyojose@users.noreply.github.com>
2022-11-01 10:25:37 +0200

    secondcommit

--- src/Pages/Add.js
+++ src/Pages/Add.js
@@ -1,24 +1,30 @@
+
+import { Container, Button } from "react-bootstrap"
+import {useState} from "react";
+
 const Add = ()=>{
     return(
-        <form className='add-form'>
-             <div className='form-control'>
+        <Container style={{backgroundColor:'#888888', width:'40%', height:'70vh'}} >
+        <form style={{marginTop:'8rem', position:'absolute', width:'30%',}} className='add-form'>
+             <div >
                 <label>ProductImage
                 </label>
                  <input type='text' placeholder='AddImage'/>
             </div>
-            <div className='form-control'>
+            <div >
                 <label>ProductName
                 </label>
                  <input type='text' placeholder='AddName'/>
             </div>
-            <div className='form-control'>
+            <div>
                 <label>ProductPrice
                 </label>
                  <input type='text' placeholder='AddPrice'/>
             </div>
-            <input type='submit' value='SaveProduct' className='btn btn-block'/>
+            <Button>Add Product</Button>
         
         </form>
+        </Container>
     )
 }
 export default Add
